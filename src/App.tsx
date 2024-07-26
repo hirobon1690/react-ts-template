6816f9ddc965396ea108f5f9b36ab07aab95491d
hirobon1690 <hirobon1690@gmail.com>
2024-07-26 19:59:13 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
